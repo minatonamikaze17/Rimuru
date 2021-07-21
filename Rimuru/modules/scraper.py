faff9c03cfa96f3d1fbb2166f8a5b138c91620cc
minatonamikaze17 <81753690+minatonamikaze17@users.noreply.github.com>
2021-07-21 09:25:18 +0530

    Update scraper.py

--- Rimuru/modules/scraper.py
+++ Rimuru/modules/scraper.py
@@ -5,7 +5,7 @@ from Rimuru import rimuru
 @rimuru.on(events.NewMessage(outgoing=True, pattern=r'^#s(.*)'))
 async def movscrape(slime):
   args = slime.message.text[2:]
-  chats = [ -1001572491465 ]
+  chats = [ -1001572491465, -1267936827 ]
   count = 0
   await slime.edit("Searching....")
   for chat in chats:
